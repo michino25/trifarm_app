0d8f7a6d4716730d043940401797ff6295973242
michittio <nguyennhutrung.nnt@gmail.com>
2023-03-31 00:31:37 +0700

    first commit

--- Quick Setup Git.bat
+++ Quick Setup Git.bat
@@ -16,7 +16,7 @@ git add .
 git commit -m "first commit"
 git branch -M main
 git remote add origin %link%
-git push -u origin main
+git push -u -f origin main
 
 echo -------------------------
 echo  First push successfully
