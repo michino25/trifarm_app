588ebae4d275a6b7428c0e9197f21f80b3aac693
NguyenHuuLoi31201024415 <loinguyen.31201024415@st.ueh.edu.vn>
2023-04-03 20:40:00 +0700

    cmt aaa

---
+++ app/src/main/java/michittio/ueh/trifarm_app/srceen/ProductDetail.java
@@ -0,0 +1,18 @@
+package michittio.ueh.trifarm_app.srceen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import michittio.ueh.trifarm_app.R;
+
+public class ProductDetail extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
